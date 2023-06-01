5557338d50eaf1039e348d0f74a843ff382c4328
utagithub <137120500@qq.com>
2023-06-01 15:52:17 +0800

    90

--- config/app.php
+++ config/app.php
@@ -3,11 +3,10 @@
 return [
 
     /*
-    /*
     |--------------------------------------------------------------------------
     | Application Name
     |--------------------------------------------------------------------------
-    |
+    |00000000
     | This value is the name of your application. This value is used when the
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
